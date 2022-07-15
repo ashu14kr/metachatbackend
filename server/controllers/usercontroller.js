dd90ee919966b466a34099bacdc200a5d7b444da
Ashutosh kumar <ashuh14th@gmail.com>
2022-07-15 22:09:44 +0530

    update

--- server/controllers/usercontroller.js
+++ server/controllers/usercontroller.js
@@ -12,7 +12,7 @@ exports.getuser = async (req, res) => {
 };
 
 exports.postuser = async (req, res) => {
-  console.log(req.file);
+  res.json(req.file);
   try {
     const data = new usermodel({
       _id: req.body._id,
