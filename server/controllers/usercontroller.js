ebf5ae4d619f2e8df01769c4a3ad88cb8b84420a
Ashutosh kumar <ashuh14th@gmail.com>
2022-07-16 11:08:43 +0530

    done

--- server/controllers/usercontroller.js
+++ server/controllers/usercontroller.js
@@ -12,7 +12,7 @@ exports.getuser = async (req, res) => {
 };
 
 exports.postuser = async (req, res) => {
-  res.send(req.files);
+  res.send(req.files.filename[1]);
   try {
     const data = new usermodel({
       _id: req.body._id,
